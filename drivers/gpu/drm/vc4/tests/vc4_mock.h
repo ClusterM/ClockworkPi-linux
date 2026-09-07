eibisidi/raspi_linux

drm/vc4: backport 27e0a194a256 and b3bf19552ea3
Get the KUnit tests passing.

Signed-off-by: Maíra Canal <mcanal@igalia.com>

drivers/gpu/drm/vc4/tests/vc4_mock.h
@@ -35,15 +35,13 @@ struct drm_plane *vc4_mock_find_plane_for_crtc(struct kunit *test,
 	return NULL;
 }
 
-struct vc4_dummy_plane {
-	struct vc4_plane plane;
-};
-
-struct vc4_dummy_plane *vc4_dummy_plane(struct kunit *test,
-					struct drm_device *drm,
-					enum drm_plane_type type);
 struct drm_plane *vc4_dummy_plane(struct kunit *test, struct drm_device *drm,
 				  enum drm_plane_type type);
+
+struct drm_plane *
+vc4_mock_atomic_add_plane(struct kunit *test,
+			  struct drm_atomic_state *state,
+			  struct drm_crtc *crtc);
 
 struct vc4_dummy_crtc {
 	struct vc4_crtc crtc;

ClusterM/ClockworkPi-linux

drivers/gpu/drm/vc4/tests/vc4_mock.h
@@ -35,15 +35,13 @@ struct drm_plane *vc4_mock_find_plane_for_crtc(struct kunit *test,
 	return NULL;
 }
 
-struct vc4_dummy_plane {
-	struct vc4_plane plane;
-};
-
-struct vc4_dummy_plane *vc4_dummy_plane(struct kunit *test,
-					struct drm_device *drm,
-					enum drm_plane_type type);
 struct drm_plane *vc4_dummy_plane(struct kunit *test, struct drm_device *drm,
 				  enum drm_plane_type type);
+
+struct drm_plane *
+vc4_mock_atomic_add_plane(struct kunit *test,
+			  struct drm_atomic_commit *state,
+			  struct drm_crtc *crtc);
 
 struct vc4_dummy_crtc {
 	struct vc4_crtc crtc;